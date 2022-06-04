5bae6491488e6f25041071ae6b5e83f08dd8739c
Nguyễn Vân Huy Vũ <59525527+vunguyen001@users.noreply.github.com>
2022-06-04 22:12:07 +0700

    a little change

---
+++ android-basics-kotlin-unscramble-app-starter/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -0,0 +1,79 @@
+package com.example.android.unscramble.ui.game
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GameViewModel: ViewModel() {
+    private var _score = MutableLiveData(0)
+    val score: LiveData<Int>
+        get() = _score
+
+    private val _currentWordCount = MutableLiveData(0)
+    val currentWordCount: LiveData<Int>
+        get() = _currentWordCount
+
+
+    private val _currentScrambledWord = MutableLiveData<String>()
+    val currentScrambledWord: LiveData<String>
+        get() = _currentScrambledWord
+
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+
+
+
+    private fun getNextWord(){
+       currentWord = allWordsList.random()
+       val tempWord = currentWord.toCharArray()
+       tempWord.shuffle()
+        while(String(tempWord).equals(currentWord,false)){
+            tempWord.shuffle()
+        }
+        if(wordsList.contains(currentWord)){
+            getNextWord()
+        }
+        else{
+            _currentScrambledWord.value = String(tempWord)
+            _currentWordCount.value = (_currentWordCount.value)?.inc()
+            wordsList.add(currentWord)
+        }
+    }
+
+    private fun increaseScore(){
+        _score.value = (_score.value)?.plus(SCORE_INCREASE)
+    }
+
+    fun isUserWordCorrect(playerWord: String): Boolean {
+        if (playerWord.equals(currentWord, true)) {
+            increaseScore()
+            return true
+        }
+        return false
+    }
+
+    fun nextWord(): Boolean {
+        return if (currentWordCount.value!! < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
+    }
+
+    init{
+        getNextWord()
+    }
+
+    fun reinitializeData() {
+        _score.value = 0
+        _currentWordCount.value = 0
+        wordsList.clear()
+        getNextWord()
+    }
+
+
+
+
+    override fun onCleared() {
+        super.onCleared()
+    }
+}
